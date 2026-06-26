abba814d866bf5842d9fa7ebf968250c76d232f2
Shuchen Zhu <feisuxiaozhu@gmail.com>
2026-06-26 20:10:01 +0000

    Add robust phase estimation for qDRIFT

--- python/src/qdk_chemistry/algorithms/registry.py
+++ python/src/qdk_chemistry/algorithms/registry.py
@@ -847,6 +847,9 @@ def _register_python_algorithms():
     from qdk_chemistry.algorithms.phase_estimation.iterative_phase_estimation import (  # noqa: PLC0415
         IterativePhaseEstimation,
     )
+    from qdk_chemistry.algorithms.phase_estimation.robust_phase_estimation import (  # noqa: PLC0415
+        RobustPhaseEstimation,
+    )
     from qdk_chemistry.algorithms.phase_estimation.standard_phase_estimation import (  # noqa: PLC0415
         StandardPhaseEstimation,
     )
@@ -893,6 +896,7 @@ def _register_python_algorithms():
     register(lambda: HadamardTest())
     register(lambda: QdkHadamardTestCircuitBuilder())
     register(lambda: StandardPhaseEstimation())
+    register(lambda: RobustPhaseEstimation())
     register(lambda: AmplitudeAmplification())
 
 
